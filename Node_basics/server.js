62250ae3d9c116cfe57e1a19654d9b3327a0c97b
Kk492000 <kunalkamthe200@gmail.com>
2024-12-31 22:10:22 +0530

    added thery file for basic nodejs, used buffer and fs core module to generate new txt file

--- Node_basics/server.js
+++ Node_basics/server.js
@@ -7,7 +7,7 @@
 const http = require("http");
 const fs = require("fs");
 
-function reqListener(req, res) {}
+// function reqListener(req, res) {}
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -22,6 +22,18 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(message, "message");
+      fs.writeFileSync("message.txt", message);
+    });
+
     fs.writeFileSync("message.txt", "DUMMY");
     res.statusCode = 302;
     res.setHeader("Location", "/");
