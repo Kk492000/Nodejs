87106710b3c1cc203944e29328c4e181a5109ec1
Kk492000 <kunalkamthe200@gmail.com>
2025-01-01 21:30:58 +0530

    added theory for npm , debugging seperated routes.js for code splitting, in launch json file added runtimeExevutable to nodemon

--- Node_basics/basic-theory.js
+++ Node_basics/basic-theory.js
@@ -31,3 +31,21 @@
 // 1)syntax errors
 // 2) runtime errors
 // 3) logical errors
+
+// module summary
+// 1)npm - npm stands for the node package manager and it allows you to manage your node project and its
+// dependencies
+// 2) you can utilize the project with the npm init
+// 3) npm scripts can be defined in the package.json file to give you shortcuts to common tasks/commands
+
+// third party packages
+// 1)node project typically dont just use the core modules and custom code but also third party packages
+// 2) you can install them via npm
+// 3) you can differentiate between the production dependencies(--save) development dependencies(--save-dev)
+// and global dependencies (-g)
+
+//debugging
+// 1) use the vs code node debugger to step into your code and go through it step by step
+// 2) analyze variable value at runtime
+// 3) look into (and manipulate ) variables at runtime
+// 4) set breakpoints cleverly(respect the async/ event driven architecture)
